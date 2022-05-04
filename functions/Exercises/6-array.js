4758842333a92e2c46939adceac105820b5b1ee1
Nic11371 <nic11371@gmail.com>
2022-05-04 20:49:26 +0300

    eslint_configure

---
+++ functions/Exercises/6-array.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const array = () => {
+	const result = [];
+	const a = x => result[x];
+	a.push = y => result.push(y);
+	a.pop = () => result.pop();
+	return a;
+};
+
+module.exports = { array }
